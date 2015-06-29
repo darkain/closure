02cef09d7d916cf79ae752aa3a61ef95906824be
Darkain (hagens) <git@darkain.com>
2015-06-29 10:11:34 -0700

    added error display

--- closure.php
+++ closure.php
@@ -2,8 +2,9 @@
 
 trait closures {
 	public function __call($function, $arguements) {
-		if (is_callable($this->$function)) {
+		if (isset($this->$function)  &&  is_callable($this->$function)) {
 			return call_user_func_array($this->$function, $arguements);
 		}
+		throw new Exception('Function not found: ' . $function);
 	}
 }
